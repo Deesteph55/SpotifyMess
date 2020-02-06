35223070ecda925f1f8f4cacfe04254e6bf60f7b
Deesteph55 <deedee704@ymail.com>
2020-02-06 14:44:51 -0600

    trying to deplo to netlify. ran npm build to make build folder

---
+++ spotify-app/build/precache-manifest.7119d9d6bbd9687f75a9523fc20556ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd3a1a2703b25490e5d66767d7194a9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dbfc41d1ff311d54f0c2",
+    "url": "/static/css/main.f759e875.chunk.css"
+  },
+  {
+    "revision": "c4461a9dd541c7ba74f8",
+    "url": "/static/js/2.cd469162.chunk.js"
+  },
+  {
+    "revision": "40247e34902848be7f40d956eb36315d",
+    "url": "/static/js/2.cd469162.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbfc41d1ff311d54f0c2",
+    "url": "/static/js/main.7234e3cf.chunk.js"
+  },
+  {
+    "revision": "761810b0cf8bfc84ffee",
+    "url": "/static/js/runtime-main.aa425418.js"
+  }
+]);
