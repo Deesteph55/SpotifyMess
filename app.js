285ba3d23deff54a1912b3acceb9fdefc27b05f4
Deesteph55 <deedee704@ymail.com>
2020-02-16 22:57:10 -0600

    trying to deploy to heroku

--- app.js
+++ app.js
@@ -45,8 +45,8 @@ app.use(express.static(__dirname + '/build'))
 if(process.env.NODE_ENV === 'production') 
 {  
   app.use(express.static(path.join(__dirname, '/build'))); 
-  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = '/build/index.html'));  
-})}
+  //app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = '/build/index.html'));  
+}
 
 app.get('/login', function(req, res) {
 
