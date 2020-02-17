9b2422ef9e6637df801bbf02b0a1efea5f27369f
Deesteph55 <deedee704@ymail.com>
2020-02-16 22:51:33 -0600

    trying to get app to run indepently on heroku

--- app.js
+++ app.js
@@ -42,6 +42,12 @@ app.use(express.static(__dirname + '/build'))
    .use(cors())
    .use(cookieParser());
 
+if(process.env.NODE_ENV === 'production') 
+{  
+  app.use(express.static(path.join(__dirname, '/build'))); 
+  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = '/build/index.html'));  
+})}
+
 app.get('/login', function(req, res) {
 
   var state = generateRandomString(16);
