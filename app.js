6bf741519f6207673b84ff8d6832ea0619577858
Deesteph55 <deedee704@ymail.com>
2020-02-19 17:37:10 -0600

    working on heroku

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ var app = express();
 if(process.env.NODE_ENV === 'production') 
 {  
   app.use(express.static(path.join(__dirname, '/build'))); 
-  app.get('*', (req, res) => { res.sendfile(path.join(__dirname = '/build/index.html')); }) 
+  app.get('/*', (req, res) => { res.sendfile(path.join(__dirname = '/build/index.html')); }) 
 }
 
 app.use(express.static(__dirname + '/build'))
