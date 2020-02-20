c02196fbae57222b2bac1683c8d44df014209c14
Deesteph55 <deedee704@ymail.com>
2020-02-20 14:15:56 -0600

    added build folder

---
+++ build/precache-manifest.a1288e6639f9a277a80a336377982cc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c13d39ca0d912ebcd9da227b14b8bc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77792bd02ab339696a8d",
+    "url": "/static/css/main.f759e875.chunk.css"
+  },
+  {
+    "revision": "30de5399ffc71b0236a1",
+    "url": "/static/js/2.1038013d.chunk.js"
+  },
+  {
+    "revision": "40247e34902848be7f40d956eb36315d",
+    "url": "/static/js/2.1038013d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "77792bd02ab339696a8d",
+    "url": "/static/js/main.36ea3a63.chunk.js"
+  },
+  {
+    "revision": "761810b0cf8bfc84ffee",
+    "url": "/static/js/runtime-main.aa425418.js"
+  }
+]);
