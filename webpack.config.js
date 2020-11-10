457a6d2881f8213821490f6dd144f3c3e0ebf42d
Deesteph55 <deedee704@ymail.com>
2020-11-10 14:10:13 -0600

    more webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,37 @@
-const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
-const path = require( 'path' );
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
 module.exports = {
-    context: __dirname + "/spotify-app",
-    entry: {
-      javascript: "./index.js",
-      html: "index.html",
-      publicPath: '/',
-    }, 
-    output: {
-      filename: "main.js",
-      path: __dirname + "/dist",
-    },
-    module: {
-        loaders: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loaders: ["babel-loader"],
-          },
-          {
-            test: /\.html$/,
-            loader: "file?name=[name].[ext]",
-          }
-        ],
+  context: __dirname + "/spotify-app",
+  entry: {
+    javascript: "./src/index.js",
+    //   html: "index.html",
+    //   publicPath: '/',
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  output: {
+    filename: "main.js",
+    path: __dirname + "/dist",
+    publicPath: '/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ["babel-loader"],
       },
-  }
+      {
+        test: /\.html$/,
+        loader: "file?name=[name].[ext]",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "public/index.html"),
+      filename: "index.html",
+    }),
+  ],
+};
